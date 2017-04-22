86a9d24ff35440d09f2a3fcd9e72dd528bde09ec
Panashe M. Fundira <contact@panashefundira.me>
2017-04-22 18:30:56 -0400

    [CIS194] Complete week 8

--- cis194/week8/src/Party.hs
+++ cis194/week8/src/Party.hs
@@ -3,7 +3,8 @@ module Party where
 
 import Data.Monoid
 import Data.Tree
-import Control.Arrow (second, first)
+import Control.Arrow (first)
+import System.IO
 import Employee
 
 glCons :: Employee -> GuestList -> GuestList
@@ -18,13 +19,25 @@ moreFun gl1@(GL _ f1) gl2@(GL _ f2)
     | f1 >= f2   = gl1
     | otherwise  = gl2
 
-treeFold :: Monoid b => (a -> [b] -> b) -> b -> Tree a -> b
-treeFold f elem Node { rootLabel = r, subForest = l } = f r (map (treeFold f elem) l)
+treeFold :: Monoid b => (a -> [b] -> b) -> Tree a -> b
+treeFold f Node { rootLabel = r, subForest = l } = f r (map (treeFold f) l)
 
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
-nextLevel emp = second (glCons emp) . foldr (<>) (mempty, mempty)
--- right : no boss
--- left : with boss
+nextLevel emp = first (glCons emp)
+                . foldr (\(inclBoss, exclBoss) (a1, a2) -> (a1 <> exclBoss, a2 <> moreFun inclBoss exclBoss))
+                (mempty, mempty)
 
 maxFun :: Tree Employee -> GuestList
-maxFun et = uncurry moreFun $ treeFold nextLevel (mempty, mempty) et
+maxFun = uncurry moreFun . treeFold nextLevel
+
+fmtGL :: GuestList -> String
+fmtGL (GL l f) = "Total fun: " ++ show f ++ "\n" ++ employeeNames
+    where employeeNames = unlines $ map (show . empName) l
+
+
+main :: IO ()
+main = do
+    handle <- openFile "company.txt" ReadMode
+    contents <- hGetContents handle
+    putStr $ fmtGL $ maxFun $ read contents
+    hClose handle
